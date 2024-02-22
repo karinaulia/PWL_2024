75af472b06a7656e68a677d4aa33fa36ed60f8f0
Karina Aulia Primanti Putri <karinauliap@gmail.com>
2024-02-22 14:19:02 +0700

    Praktikum Basic Routing

---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        <h2>Test ini dari view test</h2>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', [TestController::class, 'test']);
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function() {
+    return 'World';
+});
+
+Route::get('/about', function() {
+    return '2141762096 - Karina Aulia Primanti Putri';
 });
